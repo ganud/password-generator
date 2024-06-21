3730ee1e8f9d5b0f1937ec037f9710307237aa9d
Dundo <107737568+ganud@users.noreply.github.com>
2024-06-20 21:06:03 -0400

    Add some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import generate_password from "./password-generator";
 import "./App.css";
-import { set } from "lodash";
 
 interface Settings {
   lowercase: boolean;
@@ -30,55 +29,58 @@ function App() {
 
   return (
     <>
-      <div className="class">{password}</div>
-      a-z:{" "}
-      <input
-        type="checkbox"
-        name="lowercase"
-        onChange={() =>
-          setConfig({
-            ...config,
-            lowercase: !config.lowercase,
-          })
-        }
-        checked={config.lowercase}
-      />
-      A-Z:{" "}
-      <input
-        type="checkbox"
-        name="uppercase"
-        onChange={() =>
-          setConfig({
-            ...config,
-            uppercase: !config.uppercase,
-          })
-        }
-        checked={config.uppercase}
-      />
-      0-9:{" "}
-      <input
-        type="checkbox"
-        name="numeric"
-        onChange={() =>
-          setConfig({
-            ...config,
-            numeric: !config.numeric,
-          })
-        }
-        checked={config.numeric}
-      />
-      !@#$%^&*:{" "}
-      <input
-        type="checkbox"
-        name="symbols"
-        onChange={() =>
-          setConfig({
-            ...config,
-            symbols: !config.symbols,
-          })
-        }
-        checked={config.symbols}
-      />
+      <h1>Password Generator</h1>
+      <h3>{password}</h3>
+      <div className="flex-col">
+        a-z:{" "}
+        <input
+          type="checkbox"
+          name="lowercase"
+          onChange={() =>
+            setConfig({
+              ...config,
+              lowercase: !config.lowercase,
+            })
+          }
+          checked={config.lowercase}
+        />
+        A-Z:{" "}
+        <input
+          type="checkbox"
+          name="uppercase"
+          onChange={() =>
+            setConfig({
+              ...config,
+              uppercase: !config.uppercase,
+            })
+          }
+          checked={config.uppercase}
+        />
+        0-9:{" "}
+        <input
+          type="checkbox"
+          name="numeric"
+          onChange={() =>
+            setConfig({
+              ...config,
+              numeric: !config.numeric,
+            })
+          }
+          checked={config.numeric}
+        />
+        !@#$%^&*:{" "}
+        <input
+          type="checkbox"
+          name="symbols"
+          onChange={() =>
+            setConfig({
+              ...config,
+              symbols: !config.symbols,
+            })
+          }
+          checked={config.symbols}
+        />
+      </div>
       Length: {config.length}
       <input
         type="range"
